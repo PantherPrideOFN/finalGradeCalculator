38681c40ca58e31b89ef10b54d2560d9b37b7f09
ISRAEL TEJEDA <rl106765@rlas-macair-15.local>
2018-12-13 11:12:52 -0600

    Initial Commit

---
+++ finalGradeCalculator/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  finalGradeCalculator
+//
+//  Created by ISRAEL TEJEDA on 11/12/18.
+//  Copyright © 2018 ISRAEL TEJEDA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var yourGradeTextField: UITextField!
+    @IBOutlet weak var desiredGradeTextField: UITextField!
+    @IBOutlet weak var finalGradeTextField: UITextField!
+    
+    
+    @IBOutlet weak var finalGradeLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func onButtonTappedCalculate(_ sender: Any) {
+        
+    }
+    
+
+
+}
+
